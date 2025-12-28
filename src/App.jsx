acfcf78f1c6c8ff857289b62195f57a61fc51499
nathy <njmackinlay@gmail.com>
2025-12-27 20:20:03 -0800

    Add a couple fonts.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="bg-amber-300">This is a test of tailwind wiring.</div>
+      <div className="bg-green-400 font-goblin p-4">
+        This is a happy little font.
+      </div>
+      <div className="bg-indigo-900 font-cleric text-white p-4">
+        This is a normalish font.
+      </div>
+      <div className="bg-red-800 text-white font-warlock text-xl p-4">
+        And this is a badass font
+      </div>
     </>
   );
 }
